05e4aaf3c2e2544dfc7dc64b50395b391e1ed4dd
Kyle Luck <luck.kyle@gmail.com>
2016-06-28 08:09:29 -0400

    adding {user} is typing... functionality

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  io.emit('chat message', 'a user connected...');
+  io.emit('chat message', {user: 'System', msg: 'a user connected...'});
 
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
 
+  //emit when a user is typing
+  socket.on('typing state change', function(msg) {
+    io.emit('typing state change', msg);
+  });
+
   socket.on('disconnect', function() {
-    io.emit('chat message', 'user disconnected...');
+    io.emit('chat message', {user: 'System', msg: 'a user disconnected...'});
   });
 });
 
-http.listen(3000, function() {
-  console.log('Listening on port 3000...');
+http.listen(3001, function() {
+  console.log('Listening on port 3001...');
 });
