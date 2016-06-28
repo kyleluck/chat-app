76450e7573f95ec18385dfabe3bfa4a3535197fb
Kyle Luck <luck.kyle@gmail.com>
2016-06-28 10:25:24 -0400

    show online users functionality

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+//array of users for online status
+var users = [];
+
 // on any connection...
 io.on('connection', function(socket) {
+
+  users.push({user: null, socket: socket.id});
 
   //send user connected message to all users
   io.emit('chat message', {user: 'System', msg: 'a user connected...'});
@@ -26,7 +31,39 @@ io.on('connection', function(socket) {
   //on user disconnect, send a message to all users
   socket.on('disconnect', function() {
     io.emit('chat message', {user: 'System', msg: 'a user disconnected...'});
+
+    //search for user by socket.id and remove from users array
+    for (var j = 0; j < users.length; j++) {
+      if (users[j].socket === socket.id) {
+        io.emit('user online', {user: users[j].user, online: false});
+        users.splice(j, 1);
+        break;
+      }
+    }
+
   });
+
+  //emit message for new user coming online
+  socket.on('user online', function(msg) {
+    if (msg.online) {
+      //search for socket id in order to update the user property of the correct object in users
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].socket === socket.id) {
+          users[i].user = msg.user;
+          break;
+        }
+      }
+      io.emit('user online', {user: msg.user, online: true});
+    }
+  });
+
+  //on user connection, send all users currently online
+  users.forEach(function(user) {
+    if (user.user !== null) {
+      socket.emit('user online', {user: user.user, online: true});
+    }
+  });
+
 });
 
 //listen on port 3001
